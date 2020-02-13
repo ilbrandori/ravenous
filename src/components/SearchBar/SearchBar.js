f9aceb3c938e83306666a5bb36737bd4f603a2d8
Brando Rico <hbrico@gmail.com>
2020-02-13 12:09:57 +0100

    4th part: renders with errors

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -57,6 +57,7 @@ class SearchBar extends React.Component {
     });
   }
 
+
   render() {
     return (
       <div className="SearchBar">
@@ -70,7 +71,7 @@ class SearchBar extends React.Component {
           <input placeholder="Where?" onChange={this.handleLocationChange} />
         </div>
         <div className="SearchBar-submit">
-          <a onClick={this.handleSearch}>Let's Go</a>
+          <a href="www.#.com" onClick={this.handleSearch}>Let's Go</a>
         </div>
       </div>
     );
